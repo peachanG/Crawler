e355729a751b9548e305d9aaa598928c076398fb
peachanG <kenkman0427@gmail.com>
2019-04-07 00:33:57 +0900

    Support yaml

--- Twitter_Crawler.py
+++ Twitter_Crawler.py
@@ -1,116 +1,115 @@
-#!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
-"""
-TwitterAPIを用いてTweetを収集するプログラム
-"""
-
-import configparser
-import codecs
+"""TwitterAPIを用いてTweetを収集するプログラム."""
 import os
+import codecs
 import json
+import argparse
+
 import twitter
 
-"""
-関数がエラー終了した際のラッピング．失敗した際にデフォルト値を返す．
-@arg
-	func:実際に実行する関数
-	*args:その関数に渡す引数
-	default:その関数がエラー終了した際に返す値
-@ret
-	関数が実行可能だった場合はその結果．エラー終了した場合はデフォルト値
-"""
-def get_or_setDefault(func,*args,default=''):
-	value = default
-	try:
-		value = func(*args)
-	except configparser.NoOptionError:
-		pass
-	except:
-		pass
-	return value
+import utils.global_function as global_func
+
+
+parser = argparse.ArgumentParser(description='Twitter Crawler')
+parser.add_argument('--config', type=str, default='config/twitter.yml',
+                    help='config(yaml) file path')
+parser.add_argument('--token', '-t', type=str, default='./twitter_token.yml',
+                    help='twitter account token file path')
+parser.add_argument('--results_dir', '-r', type=str, default='results',
+                    help='results directory path')
+args = parser.parse_args()
+
 
 def main():
-	"""設定ファイルの読み込み"""
-	inifile = configparser.ConfigParser(allow_no_value = True,interpolation = configparser.ExtendedInterpolation())
-	inifile.readfp(codecs.open("./Twitter_Crawler.ini",'r','utf8'))
+    """保存用ファイルのパス"""
+    log_file_path = os.path.join(args.results_dir, "log")
+    results_text_path = os.path.join(args.results_dir, "results.txt")
+    results_json_path = os.path.join(args.results_dir, "tweets_{}.json")
 
-	"""アクセストークンの読み込み"""
-	ConsumerKey = inifile.get('tokens','ConsumerKey')
-	ConsumerSecret = inifile.get('tokens','ConsumerSecret')
-	AccessToken = inifile.get('tokens','AccessToken')
-	AccessTokenSecret = inifile.get('tokens','AccessTokenSecret')
+    """build logger"""
+    logger = global_func.build_logger(log_file_path)
 
-	"""検索パラメータの読み込み"""
-	#q = inifile.get('search_params', 'query')
-	q = "。,！,？+-\n+-笑+-「+-」+-w+-ｗ+-(+-（+-http+-https+exclude:retweets"#URL投稿やリツイートを含まないようにクエリ設定
-	geocode = get_or_setDefault(inifile.get,'search_params','geocode')
-	lang = get_or_setDefault(inifile.get,'search_params','lang')
-	locale = get_or_setDefault(inifile.get,'search_params','locale')
-	result_type = get_or_setDefault(inifile.get,'search_params','result_type')
-	count = get_or_setDefault(inifile.get,'search_params','count')
-	until = get_or_setDefault(inifile.get,'search_params','until')
-	since_id = get_or_setDefault(inifile.get,'search_params','since_id')
-	max_id = get_or_setDefault(inifile.get,'search_params','max_id')
-	include_entities = get_or_setDefault(inifile.get,'search_params','include_entities')
+    """load config"""
+    token_config = global_func.load_config(args.token)
+    config = global_func.load_config(args.config)
 
-	"""検索パラメータ(非ポスト)の読み込み"""
-	search_count = get_or_setDefault(inifile.getint,'search_params','search_count',default=1)
-	if search_count == 0:
-		search_count = True
+    """アクセストークンの読み込み."""
+    ConsumerKey = token_config['ConsumerKey']
+    ConsumerSecret = token_config['ConsumerSecret']
+    AccessToken = token_config['AccessToken']
+    AccessTokenSecret = token_config['AccessTokenSecret']
 
-	"""保存先の設定"""
-	save_dir_path = inifile.get('other_settings','Tweet_save_dir_path')
-	save_dir_name = inifile.get('other_settings','save_dir_name')
-	save_dir = os.path.join(save_dir_path,save_dir_name)
+    """検索パラメータの読み込み"""
+    q = "。,！,？+-\n+-笑+-「+-」+-w+-ｗ+-(+-（+-http+-https+exclude:retweets"  # URL投稿やリツイートを含まないようにクエリ設定
+    search_params = config['search']
+    geocode = search_params.get('geocode', '')
+    lang = search_params.get('lang', '')
+    locale = search_params.get('locale', '')
+    result_type = search_params.get('result_type', '')
+    count = search_params.get('count', 0)
+    until = search_params.get('until', '')
+    since_id = search_params.get('since_id', '')
+    max_id = search_params.get('max_id', '')
+    include_entities = search_params.get('include_entities', '')
 
-	"""「最初から」もしくは「続きから」ツイートを取得"""
-	if os.path.exists(save_dir):
-		print("Getting from the continuation.")
-		with open(os.path.join(save_dir,save_dir_name + ".txt"),'r') as file_info:
-			infos = file_info.readlines()
-			max_id = int(infos[0].split(":")[1])#maxidの設定
-			tweets_no = int(infos[1].split(":")[1])#保存時の通し番号
-			collected_data_count = int(infos[2].split(":")[1])#取得ツイート数
-	else:
-		print("Getting from the beginning.")
-		os.mkdir(save_dir)
-		tweets_no = 1#保存時の通し番号
-		collected_data_count = 0#取得ツイート数
+    """検索パラメータ(非ポスト)の読み込み"""
+    search_count = search_params.get('search_count', 1)
+    if search_count == 0:
+        search_count = True
 
-	"""検索実行"""
-	t = twitter.Twitter(auth=twitter.OAuth(AccessToken,AccessTokenSecret,ConsumerKey,ConsumerSecret),retry = True)
+    """「最初から」もしくは「続きから」ツイートを取得"""
+    if os.path.exists(args.results_dir):
+        logger.debug("Getting from the continuation.")
+        with open(results_text_path, 'r') as file_info:
+            infos = file_info.readlines()
+        max_id = int(infos[0].split(":")[1])  # maxidの設定
+        tweets_no = int(infos[1].split(":")[1])  # 保存時の通し番号
+        collected_data_count = int(infos[2].split(":")[1])  # 取得ツイート数
+    else:
+        logger.debug("Getting from the beginning.")
+        os.mkdir(args.results_dir)
+        tweets_no = 1  # 保存時の通し番号
+        collected_data_count = 0  # 取得ツイート数
 
-	while search_count:
-		try:
-			datas = t.search.tweets(q=q,geocode=geocode,lang=lang,locale=locale,result_type=result_type,count=count,until=until,since_id=since_id,max_id=max_id,include_entities=include_entities)
-		except Exception as e:
-			print(e)
-			break
-		if datas["statuses"] == []:
-			print("There are not tweets.")
-			break
+    """検索実行"""
+    t = twitter.Twitter(auth=twitter.OAuth(AccessToken, AccessTokenSecret, ConsumerKey, ConsumerSecret), retry=True)
 
-		with codecs.open(os.path.join(save_dir,"tweets"+str(tweets_no)+".json"),'w','utf8') as fo:
-			json.dump(datas["statuses"],fo,indent=4,ensure_ascii=False)
+    while search_count:
+        try:
+            datas = t.search.tweets(q=q, geocode=geocode, lang=lang, locale=locale,
+                                    result_type=result_type, count=count, until=until,
+                                    since_id=since_id, max_id=max_id, include_entities=include_entities)
 
-		tweets_no += 1#保存時の通し番号
-		collected_data_count += len(datas["statuses"])
-		print("collected %d tweets"%collected_data_count)#取得ツイート数表示
+        except Exception as e:
+            logger.debug(e)
+            break
+        if datas["statuses"] == []:
+            logger.debug("There is no tweet.")
+            break
 
-		"""次の検索時の最大max_idの設定"""
-		max_id = datas["statuses"][-1]["id"]#収集したデータからmax_idを直接取得する．next_paramsはたまに取得できないときがある？
-		max_id = str(int(max_id)-1)#検索はmax_id未満に対して行うはずなのだが，なぜか重複するため-1する．
-		# print("max_id:"+ str(max_id))#maxidの表示
+        with codecs.open(results_json_path.format(tweets_no), 'w', 'utf8') as fo:
+            json.dump(datas["statuses"], fo, indent=4, ensure_ascii=False)
 
-		with open(os.path.join(save_dir,save_dir_name + ".txt"),'w') as file_info:
-			file_info.write("next_max_id:" + str(max_id) + "\n" + "tweets_no:" + str(tweets_no) + "\n" + "collected tweets:" + str(collected_data_count))
+        tweets_no += 1  # 保存時の通し番号
+        collected_data_count += len(datas["statuses"])
+        logger.debug("collected %d tweets" % collected_data_count)  # 取得ツイート数表示
 
-		if type(search_count) is int:
-			search_count -= 1
+        """次の検索時の最大max_idの設定"""
+        max_id = datas["statuses"][-1]["id"]  # 収集したデータからmax_idを直接取得する．next_paramsはたまに取得できないときがある？
+        max_id = str(int(max_id)-1)  # 検索はmax_id未満に対して行うはずなのだが，なぜか重複するため-1する．
+        # logger.debug("max_id:"+ str(max_id))#maxidの表示
 
-	if collected_data_count == 0:
-		print("Target tweets are nothing.")
+        with open(results_text_path, 'w') as file_info:
+            file_info.write("next_max_id:" + str(max_id) + "\n" +
+                            "tweets_no:" + str(tweets_no) + "\n" +
+                            "collected tweets:" + str(collected_data_count))
+
+        if type(search_count) == int:
+            search_count -= 1
+
+        if collected_data_count == 0:
+            logger.debug("Target tweets are nothing.")
+
 
 if __name__ == "__main__":
-	main()
+    main()
---
+++ utils/global_function.py
@@ -0,0 +1,25 @@
+from logging import StreamHandler, DEBUG, Formatter, FileHandler, getLogger
+
+import yaml
+
+
+def load_config(path):
+    with open(path, 'r') as f:
+        config = yaml.load(f)
+    return config
+
+
+def build_logger(log_path):
+    logger = getLogger(__name__)
+    log_fmt = Formatter('%(asctime)s %(name)s %(lineno)d [%(levelname)s][%(funcName)s] %(message)s ')
+    handler = StreamHandler()
+    handler.setLevel('INFO')
+    handler.setFormatter(log_fmt)
+    logger.addHandler(handler)
+
+    handler = FileHandler(log_path, 'a')
+    handler.setLevel(DEBUG)
+    handler.setFormatter(log_fmt)
+    logger.setLevel(DEBUG)
+    logger.addHandler(handler)
+    return logger
